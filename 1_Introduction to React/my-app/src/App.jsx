004b0e73f8e15fff39cccb5d1eeaf3eac6231635
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-02-04 20:27:18 +0530

    add chapter 1 practical

---
+++ 1_Introduction to React/my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My First React App!</h1>
+      <p>This is a simple React project.</p>
+    </div>
+  );
+}
+
+export default App;
