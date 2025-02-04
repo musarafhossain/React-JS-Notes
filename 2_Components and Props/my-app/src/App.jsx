71c9230678cc6eb8d49bbcdea811a5bca07d6d3e
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-02-04 22:14:55 +0530

    add chapter 2 practical

---
+++ 2_Components and Props/my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import FunctionComponent from './FunctionComponenr'
+import ClassComponent from './ClassComponent'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <FunctionComponent />
+      <ClassComponent />
+    </>
+  )
+}
+
+export default App
---
+++ 2_Components and Props/my-app/src/FunctionComponenr.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FunctionComponent = () => {
+  return (
+    <div>Function Component</div>
+  )
+}
+
+export default FunctionComponent
